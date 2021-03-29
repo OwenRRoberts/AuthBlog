f1473605334a0843f746ee64dcb7e4a174fd794d
Owen Roberts <OwenRRoberts@gmail.com>
2021-03-29 08:42:29 -0500

    first commit

---
+++ src/server/config/production.ts
@@ -0,0 +1,10 @@
+export default{
+    mysql: {
+        onnectionLimit: 10,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        database: process.env.DB_SCHEMA
+    }
+
+}
